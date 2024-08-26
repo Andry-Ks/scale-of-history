8a6f8f4b6c3151f24e5a4cd58026a7b1ef73c126
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2024-08-27 00:38:56 +0300

    Initial commit

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+
+    env: {
+      backendUrl: 'https://aleksdark1313.pythonanywhere.com',
+    },
+
+  },
+});
---
+++ cypress/e2e/backend/login.cy.js
@@ -0,0 +1,81 @@
+"use strict";
+const backendUrl = Cypress.env('backendUrl');
+const validUserName = 'TestAutoUser';
+const validPassword = '12345678Abc!';
+const wrongUserName = 'WrongUserExample';
+const wrongPassword = 'wrongpassword123456789Abc!';
+
+describe ("Positive login test", () => {
+    it ("Login", () => {
+        cy.request ({
+            method: 'POST',
+            url: `${backendUrl}/login`,
+            body: {
+                user_name: validUserName,
+                password: validPassword,
+            },
+          }).then((response) => {
+            expect(response.status).to.eq(200)
+        });
+    });
+    
+});
+
+describe("Negative login tests", () => {
+    it("Login with wrong username", () => {
+        cy.request({
+            method: 'POST',
+            url: `${backendUrl}/login`,
+            body: {
+                user_name: wrongUserName,
+                password: validPassword,
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.eq(401);
+        });
+    });
+
+    it("Login with wrong password", () => {
+        cy.request({
+            method: 'POST',
+            url: `${backendUrl}/login`,
+            body: {
+                user_name: validUserName,
+                password: wrongPassword,
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.eq(401);
+        });
+    });
+
+    it("Login with missing password", () => {
+        cy.request({
+            method: 'POST',
+            url: `${backendUrl}/login`,
+            body: {
+                user_name: validUserName,
+                password: "",
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.eq(400);
+        });
+    });
+
+    it("Login with incorrect method", () => {
+        cy.request({
+            method: 'GET',
+            url: `${backendUrl}/login`,
+            body: {
+                user_name: validUserName,
+                password: validPassword,
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            expect(response.status).to.eq(405);
+        });
+    });
+
+});
