c5f68fd30c25c18fa1a6a106b626f63085f86eb8
Andry-Ks <127143251+Andry-Ks@users.noreply.github.com>
2024-09-11 20:40:32 +0300

    added tests

---
+++ cypress/e2e/frontend/homePage.cy.js
@@ -0,0 +1,8 @@
+"use strict";
+
+describe ("Main page", () => {
+    it ("Main page functionality", () => {
+        cy.visit("https://dragondark13.github.io/school_gistory_game_on_react/");
+    });
+
+});
